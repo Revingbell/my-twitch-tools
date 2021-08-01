497610a1f2c3afd0fe54346ebfb190b023848cb7
Revingbell <chade.revingbell@gmail.com>
2021-08-01 19:09:15 +0200

    V0.9.1

---
+++ correspondance.js
@@ -0,0 +1,39 @@
+var streamer = 'revingbell';
+
+var correspondanceJson = JSON.parse(`
+{
+	"videos": [
+		{
+			"command": 	"societe",
+			"file":		"societe.mp4",
+			"delay": 	"60",
+			"volume": 	"0.5"
+		},
+		{
+			"command": 	"societe2",
+			"file":		"societe.mp4",
+			"delay": 	"60",
+			"volume": 	"1"
+		},
+		{
+			"command": 	"statistique",
+			"file": 	"francois_billy_martingale.mp4",
+			"delay": 	"30",
+			"volume": 	"0.5"
+		},
+		{
+			"command": 	"findumonde",
+			"file": 	"fin_du_monde.mp4",
+			"delay": 	"10",
+			"volume": 	"0.5"
+		}
+	],
+	"sounds": [
+		{
+			"command": 	"thereyouare",
+			"file": 	"there_you_are.wav",
+			"delay": 	"30" 
+		}
+	]
+}
+`);
