2a64b33058412efe2db0956e02720b119a3e30c0
Revingbell <chade.revingbell@gmail.com>
2021-08-19 16:29:43 +0200

    Gestion des guillemets dans les commandes

--- js/myCommandEditor.js
+++ js/myCommandEditor.js
@@ -128,7 +128,7 @@ var botCommandsJson = JSON.parse(\`
     botCommands.each(function(i,e){
         let botCommandObject = {
             "commandString" :   $(e).find('.bot-command-string').val().trim(),
-            "commandOutput" :   $(e).find('.bot-command-output').val().trim().replace(/\n/g," ")
+            "commandOutput" :   $(e).find('.bot-command-output').val().trim().replace(/\n/g," ").replace(/["]/g,"\\\\$&")
         }
 
         botCommandsArray.push(botCommandObject);
